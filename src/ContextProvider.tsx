924f9058ce38afc7594bdfe4fd604ed6e1a211d2
gian <gianmarco.clu23@gmail.com>
2024-03-25 19:29:06 +0100

    feat(context): added getlaunches, getlaunchesbyid and updatelaunch functions

--- src/ContextProvider.tsx
+++ src/ContextProvider.tsx
@@ -1,9 +1,13 @@
 import { ReactNode, createContext, useState } from "react";
-import { TContext } from "./declarations";
+import { Launch, TContext } from "./declarations";
+import axios from "axios";
 
 export const AppContext = createContext<TContext>({
     launches: [],
     isLogged: false,
+    getLaunches: () => {},
+    getLaunchById: () => {},
+    updateLaunch: () => {},
     loading: false,
     error: "",
 })
@@ -13,16 +17,65 @@ interface Props {
 }
 
 export function ContextProvider({children}: Props){
-    const [ launches, setLaunches ] = useState<TContext["launches"]>([]);
+    const [ launches, setLaunches ] = useState<TContext["launches"]>(null);
     const [ isLogged, setIsLogged ] = useState<TContext["isLogged"]>(false);
     const [ loading, setLoading ] = useState<TContext["loading"]>(false);
     const [ error, setError ] = useState<TContext["error"]>("");
+
+    const getLaunches = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.get("/api/launches");
+            setLaunches(response.data);
+            setLoading(false);
+        } catch(error: any) {
+            setError(error.message);
+            setLoading(false);
+        }
+    }
+
+    const getLaunchById = async (launchId: Launch["id"]) => {
+        try {
+            setLoading(true);
+            const response = await axios.get(`/api/launches/${launchId}`);
+            setLoading(false);
+            return response.data;
+        } catch(error: any) {
+            setError(error.message);
+            setLoading(false);
+        }
+    }
+
+    const updateLaunch = async (launchId: Launch["id"], updatedLaunch: Launch) => {
+        if(error)
+        try {
+            setLoading(true);
+            const response = await axios.put(`/api/launches/${launchId}`, updatedLaunch);
+            setLoading(false);
+            setLaunches(prevLaunches => {
+                if (!prevLaunches) return null;
+                return prevLaunches.map(launch => {
+                    if (launch.id === launchId) {
+                        return updatedLaunch; // Aggiorna il lancio corrispondente
+                    } else {
+                        return launch; // Mantieni invariati gli altri lanci
+                    }
+                });
+            });
+        } catch(error: any) {
+            setError(error.message);
+            setLoading(false);
+        }
+    }
 
     return (
         <AppContext.Provider
             value={{
                 launches,
                 isLogged,
+                getLaunches,
+                getLaunchById,
+                updateLaunch,
                 loading,
                 error
             }}
