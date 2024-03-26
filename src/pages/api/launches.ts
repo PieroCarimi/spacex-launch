4f8bb5ad2173b59a6c96f9b7b915ee6a108b2832
gian <gianmarco.clu23@gmail.com>
2024-03-26 12:58:27 +0100

    feat(promise): added promises in api methods and fixed id

--- src/pages/api/launches.ts
+++ src/pages/api/launches.ts
@@ -8,59 +8,68 @@ const db = mysql.createConnection({
   database: `${process.env.DB_DATABASE}`,
 });
 
-db.connect();
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'GET') {
         if (req.query.id) {
-          const productId = req.query.id;
+          return new Promise((resolve, reject) => {
+            const productId = req.query.id;
             db.query(
-                'SELECT * FROM launches WHERE id = ?',
-                [productId],
-                (error, results) => {
-                    if (error) throw error;
-                    res.status(200).json(results);
-                }
-            );
-        } else {
-            db.query(
-                'SELECT * FROM launches',
-                (error, results) => {
+              'SELECT * FROM launches WHERE idLaunches = ?',
+              [productId],
+              (error, results) => {
                   if (error) throw error;
                   res.status(200).json(results);
-                }
+              }
             );
+          });
+        } else {
+          return new Promise((resolve, reject) => {
+            db.query(
+              'SELECT * FROM launches',
+              (error, results) => {
+                if (error) throw error;
+                res.status(200).json(results);
+              }
+          );
+          })
         }
     } else if (req.method === 'POST') {
       const { name, flight_number, data_local, success, image_small, image_large, webcast_code, details, article } = req.body;
-      db.query(
-        'INSERT INTO launches (name, flight_number, data_local, success, image_small, image_large, webcast_code, details, article ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-        [name, flight_number, data_local, success, image_small, image_large, webcast_code, details, article],
-        (error, results) => {
-          if (error) throw error;
-          res.status(201).json({ message: 'Prodotto aggiunto con successo' });
-        }
-      );
+      return new Promise((resolve, reject) => {
+        db.query(
+          'INSERT INTO launches (name, flight_number, data_local, success, image_small, image_large, webcast_code, details, article ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+          [name, flight_number, data_local, success, image_small, image_large, webcast_code, details, article],
+          (error, results) => {
+            if (error) throw error;
+            res.status(201).json({ message: 'Prodotto aggiunto con successo' });
+          }
+        );
+      });
     } else if (req.method === 'PUT') {
       const productId = req.query.id;
       const { name, flight_number, data_local, success, image_small, image_large, webcast_code, details, article } = req.body;
-      db.query(
-        'UPDATE launches SET name = ?, flight_number = ?, data_local =?, success = ?, image_small = ?, image_large = ?, webcast_code = ?, details = ?, article = ? WHERE id = ?',
-        [name, flight_number, data_local, success, image_small, image_large, webcast_code, details, article, productId],
-        (error, results) => {
-          if (error) throw error;
-          res.status(200).json({ message: 'Prodotto aggiornato con successo' });
-        }
-      );
+      return new Promise((resolve, reject) => {
+        db.query(
+          'UPDATE launches SET name = ?, flight_number = ?, data_local =?, success = ?, image_small = ?, image_large = ?, webcast_code = ?, details = ?, article = ? WHERE idLaunches = ?',
+          [name, flight_number, data_local, success, image_small, image_large, webcast_code, details, article, productId],
+          (error, results) => {
+            if (error) throw error;
+            res.status(200).json({ message: 'Prodotto aggiornato con successo' });
+          }
+        );
+      });
     } else if (req.method === 'DELETE') {
       const productId = req.query.id;
-      db.query(
-        'DELETE FROM launches WHERE id = ?',
-        [productId],
-        (error, results) => {
-          if (error) throw error;
-          res.status(200).json({ message: 'Prodotto eliminato con successo' });
-        }
-      );
+      return new Promise((resolve, reject) => {
+        db.query(
+          'DELETE FROM launches WHERE idLaunches = ?',
+          [productId],
+          (error, results) => {
+            if (error) throw error;
+            res.status(200).json({ message: 'Prodotto eliminato con successo' });
+          }
+        );
+      });
     }
 }
