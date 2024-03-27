4c99c722de89bdd533e36157e87ffa15021025fb
piero.carimi <pierocarimi@gmail.com>
2024-03-27 10:38:13 +0100

    feat(indentation): updated the indentation of all project files

--- src/ContextProvider.tsx
+++ src/ContextProvider.tsx
@@ -1,126 +1,134 @@
-import { ReactNode, createContext, useState } from "react";
-import { Launch, TContext } from "./declarations";
-import axios from "axios";
+import { ReactNode, createContext, useState } from 'react';
+import { Launch, TContext } from './declarations';
+import axios from 'axios';
 
 export const AppContext = createContext<TContext>({
-    launches: [],
-    isLogged: false,
-    getLaunches: () => {},
-    getLaunchById: () => {},
-    updateLaunch: () => {},
-    createLaunch: () => {},
-    deleteLaunch: () => {},
-    loading: false,
-    error: "",
-})
+	launches: [],
+	isLogged: false,
+	getLaunches: () => {},
+	getLaunchById: () => {},
+	updateLaunch: () => {},
+	createLaunch: () => {},
+	deleteLaunch: () => {},
+	loading: false,
+	error: '',
+});
 
 interface Props {
-    children: ReactNode;
-    initialLaunches: Launch[] | null;
+	children: ReactNode;
+	initialLaunches: Launch[] | null;
 }
 
-export function ContextProvider({children, initialLaunches}: Props){
-    const [ launches, setLaunches ] = useState<TContext["launches"]>(initialLaunches);
-    const [ isLogged, setIsLogged ] = useState<TContext["isLogged"]>(false);
-    const [ loading, setLoading ] = useState<TContext["loading"]>(false);
-    const [ error, setError ] = useState<TContext["error"]>("");
+export function ContextProvider({ children, initialLaunches }: Props) {
+	const [launches, setLaunches] =
+		useState<TContext['launches']>(initialLaunches);
+	const [isLogged, setIsLogged] = useState<TContext['isLogged']>(false);
+	const [loading, setLoading] = useState<TContext['loading']>(false);
+	const [error, setError] = useState<TContext['error']>('');
 
-    const getLaunches = async () => {
-        try {
-            setLoading(true);
-            const response = await axios.get("/api/launches");
-            setLaunches(response.data);
-            setLoading(false);
-        } catch(error: any) {
-            setError(error.message);
-            setLoading(false);
-        }
-    }
+	const getLaunches = async () => {
+		try {
+			setLoading(true);
+			const response = await axios.get('/api/launches');
+			setLaunches(response.data);
+			setLoading(false);
+		} catch (error: any) {
+			setError(error.message);
+			setLoading(false);
+		}
+	};
 
-    const getLaunchById = async (launchId: Launch["idLaunches"]) => {
-        try {
-            setLoading(true);
-            const response = await axios.get(`/api/launches/${launchId}`);
-            setLoading(false);
-            return response.data;
-        } catch(error: any) {
-            setError(error.message);
-            setLoading(false);
-        }
-    }
+	const getLaunchById = async (launchId: Launch['idLaunches']) => {
+		try {
+			setLoading(true);
+			const response = await axios.get(`/api/launches/${launchId}`);
+			setLoading(false);
+			return response.data;
+		} catch (error: any) {
+			setError(error.message);
+			setLoading(false);
+		}
+	};
 
-    const updateLaunch = async (launchId: Launch["idLaunches"], updatedLaunch: Launch) => {
-        if(error)
-        try {
-            setLoading(true);
-            const response = await axios.put(`/api/launches/${launchId}`, updatedLaunch);
-            setLoading(false);
-            setLaunches(prevLaunches => {
-                if (!prevLaunches) return null;
-                return prevLaunches.map(launch => {
-                    if (launch.idLaunches === launchId) {
-                        return updatedLaunch; // Aggiorna il lancio corrispondente
-                    } else {
-                        return launch; // Mantieni invariati gli altri lanci
-                    }
-                });
-            });
-        } catch(error: any) {
-            setError(error.message);
-            setLoading(false);
-        }
-    }
+	const updateLaunch = async (
+		launchId: Launch['idLaunches'],
+		updatedLaunch: Launch,
+	) => {
+		if (error)
+			try {
+				setLoading(true);
+				const response = await axios.put(
+					`/api/launches/${launchId}`,
+					updatedLaunch,
+				);
+				setLoading(false);
+				setLaunches((prevLaunches) => {
+					if (!prevLaunches) return null;
+					return prevLaunches.map((launch) => {
+						if (launch.idLaunches === launchId) {
+							return updatedLaunch; // Aggiorna il lancio corrispondente
+						} else {
+							return launch; // Mantieni invariati gli altri lanci
+						}
+					});
+				});
+			} catch (error: any) {
+				setError(error.message);
+				setLoading(false);
+			}
+	};
 
-    const createLaunch = async (newLaunch: Launch) => {
-        try {
-            setLoading(true);
-            await axios.post("/api/launches", newLaunch);
-            setLoading(false);
-            if (launches === null) {
-                setLaunches([newLaunch])
-            } else {
-                setLaunches([...launches, newLaunch]) 
-            }
-        } catch (error: any) {
-            setError(error.message);
-            setLoading(false)
-        }
-    }
+	const createLaunch = async (newLaunch: Launch) => {
+		try {
+			setLoading(true);
+			await axios.post('/api/launches', newLaunch);
+			setLoading(false);
+			if (launches === null) {
+				setLaunches([newLaunch]);
+			} else {
+				setLaunches([...launches, newLaunch]);
+			}
+		} catch (error: any) {
+			setError(error.message);
+			setLoading(false);
+		}
+	};
 
-    const deleteLaunch = async (launchId: number) => {
-        try {
-            setLoading(true);
-            await axios.delete(`/api/launches?id=${launchId}`);
-            setLaunches((prevLaunches:  Array<Launch> | null) =>{
-                if (prevLaunches !== null){
-                    return prevLaunches.filter((launch: Launch) => launch.idLaunches !== launchId);
-                } else {
-                    return null;
-                }
-            }
-            );
-            setLoading(false);
-        } catch (error: any) {
-            setError(error.message);
-            setLoading(false);
-        }
-    };
+	const deleteLaunch = async (launchId: number) => {
+		try {
+			setLoading(true);
+			await axios.delete(`/api/launches?id=${launchId}`);
+			setLaunches((prevLaunches: Array<Launch> | null) => {
+				if (prevLaunches !== null) {
+					return prevLaunches.filter(
+						(launch: Launch) => launch.idLaunches !== launchId,
+					);
+				} else {
+					return null;
+				}
+			});
+			setLoading(false);
+		} catch (error: any) {
+			setError(error.message);
+			setLoading(false);
+		}
+	};
 
-    return (
-        <AppContext.Provider
-            value={{
-                launches,
-                isLogged,
-                getLaunches,
-                getLaunchById,
-                updateLaunch,
-                createLaunch,
-                deleteLaunch,
-                loading,
-                error
-            }}
-        >
-            {children}
-        </AppContext.Provider>
-    )
+	return (
+		<AppContext.Provider
+			value={{
+				launches,
+				isLogged,
+				getLaunches,
+				getLaunchById,
+				updateLaunch,
+				createLaunch,
+				deleteLaunch,
+				loading,
+				error,
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	);
 }
