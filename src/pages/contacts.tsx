e7eceb6d7f43b751e1f70d386c6930966b7ff20e
piero.carimi <pierocarimi@gmail.com>
2024-04-04 18:50:06 +0200

    feat(contacts): added contacts page and added background image

---
+++ src/pages/contacts.tsx
@@ -0,0 +1,11 @@
+export default function Contacts() {
+    return (
+        <div className="h-full flex flex-col justify-between bg-[url('https://www.pixelstalk.net/wp-content/uploads/images6/Dark-Universe-Wallpaper-HD.jpg')] bg-cover bg-center bg-no-repeat" style={{height: "calc(100vh - 180px)", textAlign: "center"}}>
+            <div className="flex-grow flex items-center justify-center">
+                <p className="text-white text-6xl font-bold">
+                    <span ></span>
+                </p>
+            </div>
+        </div>
+    );
+}
