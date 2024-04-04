d05bea591e7a8489611b01d3fd94b79773314be2
gian <gianmarco.clu23@gmail.com>
2024-04-04 11:24:37 +0200

    feat(cardsmall): modified card borders

--- src/components/CardSmall.tsx
+++ src/components/CardSmall.tsx
@@ -20,17 +20,17 @@ export default function CardSmall({ launch }: { launch: Launch }) {
     }
 
     return (
-        <div className="max-w-sm relative rounded overflow-hidden shadow-lg w-full">
+        <div className="max-w-sm relative rounded-lg overflow-hidden shadow-lg w-full border border-gray-400">
             <img
                 className="w-full"
                 src={launch.image_small}
                 alt="Image patch"
             />
-            <div className="px-6 py-4 min-h-28 max-h-28">
+            <div className="px-6 py-4 min-h-28 max-h-28 bg-white">
                 <div className="font-bold text-xl mb-2">{launch.name}</div>
                 <p className="text-gray-700 text-base line-clamp-3">{launch.details}</p>
             </div>
-            <div className="px-4 pt-4">
+            <div className="px-4 pt-4 bg-white">
                 <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                     Launch {launch.flight_number}
                 </span>
@@ -53,7 +53,7 @@ export default function CardSmall({ launch }: { launch: Launch }) {
                     {new Date(launch.data_local).toLocaleDateString()}
                 </span>
             </div>
-            <div className="px-6 pt-3 pb-3">
+            <div className="px-6 pt-3 pb-3 bg-white">
                 <button className="bg-transparent hover:bg-gray-800 text-gray-800 font-semibold hover:text-white py-2 px-4 border border-gray-800 hover:border-transparent rounded" onClick={onClickDetails}>
                     Details
                 </button>
